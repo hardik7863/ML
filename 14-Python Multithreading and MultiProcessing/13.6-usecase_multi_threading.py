644b585fb72a1a8acf25a9115495bd3fb87e20a4
hardik7863 <hardikbatwal@gmail.com>
2024-09-20 09:17:40 +0530

    uploading files

--- 14-Python Multithreading and MultiProcessing/13.6-usecase_multi_threading.py
+++ 14-Python Multithreading and MultiProcessing/13.6-usecase_multi_threading.py
@@ -27,3 +27,18 @@ urls=[
 
 ,"https://python.langchain.com/v0.2/docs/tutorials/"
 ]
+
+def fetch_content(url):
+    response=requests.get(url)
+    soup=BeautifulSoup(response.content,'html.parser')
+    print(f'Fetched {len(soup.text)} characters from {url}')
+threads=[]
+
+for url in urls:
+    thread=threading.Thread(target=fetch_content,args=(url,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+print("all web pages fetched")
---
+++ 16-Flask/flaskApps/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+"""
+It creates an instance of the flask class,
+which will be your WSGI (Web Server Gateway Interface)application.
+
+"""
+
+app=Flask()
